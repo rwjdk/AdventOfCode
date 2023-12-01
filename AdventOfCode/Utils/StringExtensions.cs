5893bcaa5208d3faa21d21f8c18e27d4cf64e1b1
Rasmus Wulff Jensen <rasmus.wulff.jensen@gmail.com>
2023-12-01 07:39:34 +0100

    Done

--- AdventOfCode/Utils/StringExtensions.cs
+++ AdventOfCode/Utils/StringExtensions.cs
@@ -1,4 +1,6 @@
-﻿namespace Utils;
+﻿using System.Diagnostics;
+
+namespace Utils;
 
 public static class StringExtensions
 {
@@ -10,5 +12,87 @@ public static class StringExtensions
     public static bool IsNullOrWhiteSpace(this string input)
     {
         return string.IsNullOrWhiteSpace(input);
+    }
+
+    public static int GetFirstDigitInString(this string input, bool includeSpelledOutDigits)
+    {
+        var digits = includeSpelledOutDigits ? NormalAndSpelledOutDigits : NormalDigits;
+        KeyValuePair<Digit, int> lowestIndexDigit = new(new Digit(0), int.MaxValue);
+        foreach (var digit in digits)
+        {
+            var indexOf = input.IndexOf(digit.StringRepresentation, StringComparison.OrdinalIgnoreCase);
+            if (indexOf != -1 && indexOf < lowestIndexDigit.Value)
+            {
+                lowestIndexDigit = new(digit, indexOf);
+            }
+        }
+        return lowestIndexDigit.Key.Value != 0 ? lowestIndexDigit.Key.Value : throw new ArgumentException("No digits in string");
+    }
+
+    public static int GetLastDigitInString(this string input, bool includeSpelledOutDigits)
+    {
+        var digits = includeSpelledOutDigits ? NormalAndSpelledOutDigits : NormalDigits;
+        KeyValuePair<Digit, int> highestIndexDigit = new(new Digit(0), int.MinValue);
+        foreach (var digit in digits)
+        {
+            var indexOf = input.LastIndexOf(digit.StringRepresentation, StringComparison.OrdinalIgnoreCase);
+            if (indexOf != -1 && indexOf > highestIndexDigit.Value)
+            {
+                highestIndexDigit = new(digit, indexOf);
+            }
+        }
+        return highestIndexDigit.Key.Value != 0 ? highestIndexDigit.Key.Value : throw new ArgumentException("No digits in string");
+    }
+
+    private static readonly Digit[] NormalDigits = {
+        new(1),
+        new(2),
+        new(3),
+        new(4),
+        new(5),
+        new(6),
+        new(7),
+        new(8),
+        new(9),
+    };
+
+    private static readonly Digit[] NormalAndSpelledOutDigits = {
+        new(1),
+        new(2),
+        new(3),
+        new(4),
+        new(5),
+        new(6),
+        new(7),
+        new(8),
+        new(9),
+        new("one", 1),
+        new("two", 2),
+        new("three", 3),
+        new("four", 4),
+        new("five", 5),
+        new("six", 6),
+        new("seven", 7),
+        new("eight", 8),
+        new("nine", 9)
+    };
+
+    [DebuggerDisplay("{Value} '{StringRepresentation}'")]
+    private class Digit
+    {
+        public string StringRepresentation { get; }
+        public int Value { get; }
+
+        public Digit(int value)
+        {
+            StringRepresentation = value.ToString();
+            Value = value;
+        }
+
+        public Digit(string stringRepresentation, int value)
+        {
+            StringRepresentation = stringRepresentation;
+            Value = value;
+        }
     }
 }
--- AdventOfCode/Year2023/Day01.cs
+++ AdventOfCode/Year2023/Day01.cs
@@ -1,18 +1,43 @@
+using Utils;
+
 namespace Year2023;
 
 public class Day01
 {
+    //On each line, the calibration value can be found by combining the first digit and the last digit (in that order) to form a single two-digit number.
+    //In Sample values of these four lines are 12, 38, 15, and 77. Adding these together produces 142.
+    //What is the sum of all of the calibration values?
     [Theory]
-    [InlineData("Day01_Sample.txt")]
-    public void Part1(string inputFile)
+    [InlineData("Day01_Sample1.txt", 142)]
+    [InlineData("Day01_Input.txt", 55712)]
+    public void Part1(string inputFile, int expectedTotal)
     {
-
+        Assert.Equal(expectedTotal, GetSum(inputFile, false));
     }
 
+    //It looks like some of the digits are actually spelled out with letters: one, two, three, four, five, six, seven, eight, and nine also count as valid "digits".
+    //What is the sum of all of the calibration values with the "extra" special digits
     [Theory]
-    [InlineData("Day01_Sample.txt")]
-    public void Part2(string inputFile)
+    [InlineData("Day01_Sample2.txt", 281)]
+    [InlineData("Day01_Input.txt", 55413)]
+    public void Part2(string inputFile, int expectedTotal)
     {
-
+        Assert.Equal(expectedTotal, GetSum(inputFile, true));
     }
+
+    private int GetSum(string inputFile, bool includeSpelledOutDigits)
+    {
+        var inputLines = InputReader.ReadInputLines(inputFile);
+        int sum = 0;
+        foreach (var inputLine in inputLines)
+        {
+            var first = inputLine.GetFirstDigitInString(includeSpelledOutDigits).ToString();
+            var last = inputLine.GetLastDigitInString(includeSpelledOutDigits).ToString();
+            var sumAsString = string.Concat(first, last);
+            sum += sumAsString.ToInteger();
+        }
+        return sum;
+    }
+    
+    
 }
