5d7d1a644023ad482f9e9bfd5323e7db5465c590
Rasmus Wulff Jensen <rasmus.wulff.jensen@gmail.com>
2023-12-03 13:28:43 +0100

    Tweaks

--- AdventOfCode/Year2023/Day01.cs
+++ AdventOfCode/Year2023/Day01.cs
@@ -6,7 +6,7 @@ namespace Year2023;
 //https://adventofcode.com/2023/day/1
 public class Day01
 {
-    private const string Day = "Day01"; //TODO add day
+    private const string Day = "Day01";
 
     //On each line, the calibration value can be found by combining the first digit and the last digit (in that order) to form a single two-digit number.
     //In Sample values of these four lines are 12, 38, 15, and 77. Adding these together produces 142.
